8a748f53587ee6b75885fe81a689cfc1da2d6bf5
NEW-BORN <manavbhullar341@gmail.com>
2024-03-23 18:45:35 +0530

    Delete unnecessary files and code

---
+++ Card_components/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card" >
+            <img src="https://via..placeholder.com/150" alt="Profile picture"></img>
+            <h2>Manav</h2>
+            <p>I'm a student</p>
+        </div>
+    );
+}
+
+export default Card;
