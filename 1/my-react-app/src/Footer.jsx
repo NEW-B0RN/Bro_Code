df3eded3ad349b00c699770b1662568cadeed680
NEW-BORN <manavbhullar341@gmail.com>
2024-03-23 18:10:51 +0530

    Fix formatting issues in code

---
+++ 1/my-react-app/src/Footer.jsx
@@ -0,0 +1,12 @@
+
+function footer(){
+    return(
+        <>
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Manav</p>
+        </footer>
+        </>
+    );
+}
+
+export default footer;
---
+++ 1/my-react-app/src/Header.jsx
@@ -0,0 +1,21 @@
+function Header(){
+
+    return(
+        <>
+        <header>
+            <h1>Manav's Web</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href=".">Home</a></li>
+                <li><a href=".">Contact</a></li>
+                <li><a href=".">Services</a></li>
+                <li><a href=".">About</a></li>
+            </ul>
+        </nav>
+        <hr />
+        </>
+    );
+}
+
+export default Header;
