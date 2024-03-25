feaa6dc25df920afa857af82e89a407b5de84e15
NEW-BORN <manavbhullar341@gmail.com>
2024-03-25 19:26:13 +0530

    Refactor list rendering to use props

--- list_rendering/src/App.jsx
+++ list_rendering/src/App.jsx
@@ -1,8 +1,14 @@
 import List from "./List.jsx"
 
 function App() {
+
+  const fruits = [{id:1, name:"apple", calories:95},
+                  {id: 2, name:"banana", calories:105},
+                  {id : 3,name:"durian", calories:375},
+                  {id : 4,name:"eggplant", calories:25},
+                  {id: 5, name:"cherry", calories:50}];
     return (
-         <List />
+         <List items={fruits} category="Fruits"/>
     );
 }
 
--- list_rendering/src/List.jsx
+++ list_rendering/src/List.jsx
@@ -1,17 +1,14 @@
-function List(){
-    const fruits = [{id:1, name:"apple", calories:95},
-                    {id: 2, name:"banana", calories:105},
-                    {id : 3,name:"durian", calories:375},
-                    {id : 4,name:"eggplant", calories:25},
-                    {id: 5, name:"cherry", calories:50}];
+function List(props){
 
 // Sort fruits by name
     //fruits.sort((a,b)=> a.name.localeCompare(b.name));
 
 // Sort fruits by calories
-    fruits.sort((a,b)=> a.calories-b.calories);
+    //fruits.sort((a,b)=> a.calories-b.calories);
+    const itemList = props.items;
 
-    const listItems = fruits.map(fruit => <li key={fruit.id} >{fruit.name} has <b>{fruit.calories}</b> calories</li>);
+
+    const listItems = itemList.map(item => <li key={item.id} >{item.name} has <b>{item.calories}</b> calories</li>);
     return(<ul>{listItems}</ul>);
 }
 
